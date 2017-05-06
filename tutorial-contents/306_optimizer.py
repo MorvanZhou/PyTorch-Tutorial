f6eeaa61d10b8990e59a2a03b6c554e5e26a5d17
Morvan Zhou <mz@email.com>
2017-05-06 22:53:23 +1000

    all english

--- tutorial-contents/306_optimizer.py
+++ tutorial-contents/306_optimizer.py
@@ -1,5 +1,5 @@
 """
-Know more, visit my tutorial page: https://morvanzhou.github.io/tutorials/
+View more, visit my tutorial page: https://morvanzhou.github.io/tutorials/
 My Youtube Channel: https://www.youtube.com/user/MorvanZhou
 
 Dependencies:

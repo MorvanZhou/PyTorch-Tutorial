c711da855a28319bc9a839f1e169a386022e3e52
Morvan Zhou <morvanzhou@gmail.com>
2018-05-10 08:48:03 +1000

    update to torch 0.4

--- tutorial-contents/305_batch_train.py
+++ tutorial-contents/305_batch_train.py
@@ -16,7 +16,7 @@ BATCH_SIZE = 5
 x = torch.linspace(1, 10, 10)       # this is x data (torch tensor)
 y = torch.linspace(10, 1, 10)       # this is y data (torch tensor)
 
-torch_dataset = Data.TensorDataset(data_tensor=x, target_tensor=y)
+torch_dataset = Data.TensorDataset(x, y)
 loader = Data.DataLoader(
     dataset=torch_dataset,      # torch TensorDataset format
     batch_size=BATCH_SIZE,      # mini batch size
